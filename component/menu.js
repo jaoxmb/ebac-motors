a2ee48440f7c929bbcccbae7005927ad0a00ebc9
jaoxmb <joaovictorcgdossantos@gmail.com>
2023-10-10 21:24:11 -0300

    Chore: improved menu display code

---
+++ component/menu.js
@@ -0,0 +1,3 @@
+export const menuToggle = () => {
+  $("#menu").slideToggle();
+}
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import { handle_menu } from "./component/menu-hamburger.js"
+import { menuToggle } from "./component/menu.js"
 
 $(document).ready(() => {
   const hamburger = $("#hamburger");
   const slide = $("#slide");
 
   hamburger.on("click", () => {
-    handle_menu();
+    menuToggle();
   });
 
   slide.slick({
