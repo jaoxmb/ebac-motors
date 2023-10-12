7bc19986e46e200f8fc675b18d16b26117c79258
jaoxmb <joaovictorcgdossantos@gmail.com>
2023-10-12 14:36:49 -0300

    Feat: added jQuery Validation plugin for contact form validation

--- component/contact.js
+++ component/contact.js
@@ -7,17 +7,47 @@ const message = $("#message");
 
 tel.mask("(00) 00000-0000");
 
-
-form.on("submit", (e) => {
-  e.preventDefault();
-
-  const contact = {
-    name: name.val(),
-    email: email.val(),
-    tel: tel.val(),
-    vehicle: vehicle.val(),
-    message: message.val()
+form.validate({
+	rules: {
+		name: {
+			required: true,
+		},
+		email: {
+			required: true,
+      email: true
+		},
+    tel: {
+      required: true,
+      minlength: 15
+    },
+    vehicle: {
+      required: true
+    },
+    message: {
+      required: true
+    }
+	},
+	messages: {
+		name: "Você deve inserir o seu nome.",
+		email: {
+			required: "Você deve inserir um e-mail para entrarmos em contato.",
+			email: "Você deve inserir um e-mail valido."
+		},
+    tel: "Você deve inserir um telefone vaálido.",
+    vehicle: "Você deve inserir o seu veiculo de interesse.",
+    message: "Você deve inserir uma mensagem."
+	},
+  submitHandler: (form) => {
+    const contact = {
+      name: name.val(),
+      email: email.val(),
+      tel: tel.val(),
+      vehicle: vehicle.val(),
+      message: message.val()
+    }
+    console.log(contact);
+    
+    form.reset();
+    alert("Obrigado pelo contato! Responderemos assim que possivel.");
   }
-
-  console.log(contact);
 })
