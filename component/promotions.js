3e15775c8e04f524a2440aa20bd209e5452b415f
jaoxmb <joaovictorcgdossantos@gmail.com>
2023-10-12 15:35:18 -0300

    Feat: added old price parameter to shop

--- component/promotions.js
+++ component/promotions.js
@@ -5,18 +5,21 @@ const itensMock = [
     img: "/images/carro-bmw.png",
     vehicle: "Carro BMW",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    oldPrice: 90000,
     price: 158000
   },
   {
     img: "/images/fusca.png",
     vehicle: "Carro Fusca",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    oldPrice: 90000,
     price: 32000
   },
   {
     img: "/images/moto-bmw.png",
     vehicle: "Moto BMW",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    oldPrice: 90000,
     price: 62000
   }
 ];
--- component/shop.js
+++ component/shop.js
@@ -9,14 +9,17 @@ export const shop = (elementSelector, itens) => {
   const shop = $(elementSelector);
 
   itens.forEach((item) => {
-    const {img, vehicle, desc, price} = item;
+    const {img, vehicle, desc, price, oldPrice} = item;
     $(`
       <li>
         <img src="${img}" alt="${vehicle}"/>
         <div>
           <h3>${vehicle}</h3>
           <p>${desc}</p>
-          <span>${currency(price)}</span>
+          <div class="price">
+            ${oldPrice? `<del>${currency(oldPrice)}</del>` : ''}
+            <strong>${currency(price)}</strong>
+          </div>
           <button class="select-vehicle" data-vehicle="${vehicle}">Tenho interesse</button>
         </div>
       </li>
